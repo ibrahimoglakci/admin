1ec0d023cbcc488d0952b0eb22e614234175716a
Ibrahim Oglakci <contact@ibrahimoglakci.com>
2023-10-10 15:08:54 +0300

    Updated Language System

--- front-end/includes/contact-settings.php
+++ front-end/includes/contact-settings.php
@@ -28,7 +28,7 @@ if ($values) {
                 </div>
             </div>
             <!-- end page title -->
-
+            
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
